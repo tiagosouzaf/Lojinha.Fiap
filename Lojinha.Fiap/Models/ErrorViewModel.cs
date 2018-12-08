29f1ce96b8d9b271729ddd76d08185295f530a9f
Logon Aluno <logonrmlocal@fiap.com.br>
2018-12-08 09:54:02 -0200

    Add project files.

---
+++ Lojinha.Fiap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lojinha.Fiap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
